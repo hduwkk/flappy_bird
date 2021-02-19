e999d4d49f3b1f3d79b6915a95210e46b8becd5d
hduwkk <873057692@qq.com>
2021-02-19 16:51:35 +0800

    ...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import util from './js/util'
 import { getGameScene } from './js/gameScene'
-import { getOverScene } from './js/overScene'
+import { bindMaskEvent, gameover } from './js/overScene'
 
 import bridImg from './imgs/bird.png'
 import landImg from './imgs/land.png'
@@ -22,7 +22,6 @@ console.log('images ... ...')
 
 util.loadImage(images, init)
 
-let run
 function init (imgObj) {
   console.log('init .. ..')
   // 根据背景的大小设置画布的大小
@@ -31,31 +30,15 @@ function init (imgObj) {
 
   // 创建游戏场景
   const gameScene = getGameScene(ctx, imgObj)
-  // 创建游戏结束场景
-  const overScene = getOverScene(ctx)
 
   // 添加小鸟死亡的听众
-  gameScene.addListener(function () {
-    console.log('gameScene: bird die')
-    window.isRun = false // 游戏场景退场
-    overScene.draw() // 结束场景切入
-    document.body.style.backgroundColor = '#7c7b6e'
-  })
+  // '#7c7b6e'
+  gameScene.addListener(gameover)
 
-  run = function () {
+  function run () {
     gameScene.draw()
     window.isRun && requestAnimationFrame(run)
   }
   run()
-}
-
-let btn = document.getElementById('start')
-btn.onclick = function () {
-  if (!window.isRun) {
-    window.isRun = true
-    run()
-  }
-  console.log(this)
-  btn.parentElement.remove()
-  btn = null
+  bindMaskEvent(run)
 }
--- src/js/overScene.js
+++ src/js/overScene.js
@@ -1,26 +1,47 @@
-const w = window.innerWidth;
-const h = window.innerHeight;
-class OverScene {
-  constructor(ctx) {
-    this.ctx = ctx;
-  }
-  draw() {
-    // 为了防止影响全局状态，所以先save再restore
-    this.ctx.save();
+// const w = window.innerWidth;
+// const h = window.innerHeight;
+const startBtn = document.getElementById('start');
+const againBtn = document.getElementById('again');
+const maskEle = document.getElementById('mask')
 
-    const canvasWidth = this.ctx.canvas.width;
-    const canvasHeight = this.ctx.canvas.height;
-    this.ctx.fillStyle = 'rgba( 100, 100, 100, 0.8 )';
-    this.ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-    this.ctx.textAlign = 'center';
-    this.ctx.textBaseline = 'middle';
-    this.ctx.fillStyle = 'red';
-    this.ctx.font = '900 40px 微软雅黑';
-    this.ctx.fillText('GAME OVER!!!', (w < canvasWidth ? w : canvasWidth) / 2, (h > canvasHeight ? h : canvasHeight) / 2);
-    this.ctx.restore();
+export function bindMaskEvent (startFn) {
+  // 游戏开始
+  startBtn.onclick = () => {
+    maskEle.classList.add('running')
+    if (!window.isRun) {
+      window.isRun = true
+      startFn()
+    }
+  }
+// 重新开始
+  againBtn.onclick = () => {
+    window.location.reload()
   }
 }
 
-export function getOverScene(ctx) {
-  return new OverScene(ctx);
+export function gameover () {
+  maskEle.classList.remove('running')
+  maskEle.classList.add('game-over')
 }
+
+
+// class OverScene {
+//   constructor(ctx) {
+//     this.ctx = ctx;
+//   }
+//   draw() {
+//     // 为了防止影响全局状态，所以先save再restore
+//     this.ctx.save();
+
+//     const canvasWidth = this.ctx.canvas.width;
+//     const canvasHeight = this.ctx.canvas.height;
+//     this.ctx.fillStyle = 'rgba( 100, 100, 100, 0.8 )';
+//     this.ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+//     this.ctx.textAlign = 'center';
+//     this.ctx.textBaseline = 'middle';
+//     this.ctx.fillStyle = 'red';
+//     this.ctx.font = '900 40px 微软雅黑';
+//     this.ctx.fillText('GAME OVER!!!', (w < canvasWidth ? w : canvasWidth) / 2, (h > canvasHeight ? h : canvasHeight) / 2);
+//     this.ctx.restore();
+//   }
+// }
